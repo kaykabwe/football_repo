a7b4287e38cab761c08897b5386d741cceaf07d8
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-01-19 03:30:43 +0200

    sixth commit: added apps urls file

--- footballsite/urls.py
+++ footballsite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('footballsite', include('teaminfors.urls') ),
+    path('footballsite/', include('teaminfors.urls') ),
     path('admin/', admin.site.urls),
 ]
