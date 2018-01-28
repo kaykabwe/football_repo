c42da2d83d2b5f92e731c58a7254a56dd1f926eb
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-01-28 17:20:23 +0200

    tenth commit maybe: lost count - added classes to model to represent tables - constraints not added yet

--- teaminfors/models.py
+++ teaminfors/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class LeagueTable(models.Model):
+    position
+    team_name
+    matches_played
+    lost
+    drawn
+    won
+    goals_for
+    goals_against
+
+class Team(models.Model):
+    name 
+    city
+    address
+    established_date
+
+class News(models.Model):
+    news_id
+    teams_ tagged
+    date_issued
+    read_content
+    
