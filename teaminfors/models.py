a86634a20144d88fe6e20f6005d22c227a8a54b8
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-01-30 09:11:21 +0200

    commit 30 January 2018 - edited models.py

--- teaminfors/models.py
+++ teaminfors/models.py
@@ -4,23 +4,33 @@ from django.db import models
 
 class LeagueTable(models.Model):
     position
-    team_name
-    matches_played
-    lost
-    drawn
-    won
-    goals_for
-    goals_against
+    team_name = models.CharField(max_length=60)
+    matches_played = models.IntegerField
+    lost = models.IntegerField
+    drawn = models.IntegerField
+    won = models.IntegerField
+    goals_for = models.IntegerField
+    goals_against = models.IntegerField
+
+    # to show readable string of class 
+    def __str__(self):
+        return self.question_text
 
 class Team(models.Model):
-    name 
-    city
-    address
-    established_date
+    name = models.CharField(max_length=60)
+    city = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    est_date = models.DateTimeField('established date')
+
+    def __str__(self):
+        return self.question_text
 
 class News(models.Model):
-    news_id
-    teams_ tagged
-    date_issued
-    read_content
+    news_id = models.IntegerField
+    teams_tagged = models.CharField(max_length=60)
+    date_issued = est_date = models.DateTimeField('Date\
+     news article is published')
+    read_content = models.TextField
     
+    def __str__(self):
+        return self.question_text
