954aa2df91d17dab5b827e34e6c8a5301cf79d06
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-02-05 17:12:54 +0200

    thirteenth commit

--- teaminfors/models.py
+++ teaminfors/models.py
@@ -3,34 +3,37 @@ from django.db import models
 # Create your models here.
 
 class LeagueTable(models.Model):
-    position = models.IntegerField
-    team_name = models.CharField(max_length=60)
-    matches_played = models.IntegerField
-    lost = models.IntegerField
-    drawn = models.IntegerField
-    won = models.IntegerField
-    goals_for = models.IntegerField
-    goals_against = models.IntegerField
+    position = models.IntegerField()
+    # team_name = models.CharField(max_length=80)
+    team_name = models.ForeignKey(Team, on_delete=models.CASCADE)
+    matches_played = models.IntegerField(null=False)    # null is false by default
+    lost = models.IntegerField(null=False)
+    drawn = models.IntegerField(null=False)
+    won = models.IntegerField(null=False)
+    goals_for = models.IntegerField(null=False)
+    goals_against = models.IntegerField(null=False)
 
     # to show readable string of class 
     def __str__(self):
-        return self.question_text
+        return "This is the league table"
 
 class Team(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=80)
     city = models.CharField(max_length=20)
-    address = models.CharField(max_length=200)
+    email = models.EmailField()
+    address = models.TextField()
     est_date = models.DateTimeField('established date')
 
     def __str__(self):
-        return self.question_text
+        return self.name
 
 class News(models.Model):
-    news_id = models.IntegerField
-    teams_tagged = models.CharField(max_length=60)
+    news_id = models.IntegerField()
+    # teams_tagged = models.CharField(max_length=80)
+    team_tagged = models.ForeignKey(Team, on_delete=models.CASCADE)
     date_issued = est_date = models.DateTimeField('Date\
      news article is published')
-    read_content = models.TextField
+    read_content = models.TextField()
     
     def __str__(self):
-        return self.question_text
+        return self.news_id
