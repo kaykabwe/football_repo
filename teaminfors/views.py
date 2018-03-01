d15471e3f2c89b74631a18f54b07ef4d87a136de
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-03-01 15:16:57 +0200

    test commit with edited files

--- teaminfors/views.py
+++ teaminfors/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 from django.http import HttpResponse
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'test_dict'
+'''
 def index(request):
-    return HttpResponse("Hello, you at the Zed soccer site.")
+    test_string = "Zambia football test"
+    context = {'test': test_string,}
+    return render(request,'teaminfors/index.html', context)
 
 def details(request, team = "kaunda FC"):
     response = "You are looking at the page of %s football club"
@@ -12,3 +19,4 @@ def details(request, team = "kaunda FC"):
 def newscorner(request):
     introduction = "Latest news in League"
     return HttpResponse(introduction)
+'''
